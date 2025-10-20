9b9ba33a68fb9c45be7a462fe91209f2e00bc9c4
kosterseb <sebkos.code@gmail.com>
2025-10-20 12:40:01 +0200

    added the authentication page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // Components import
 import Home from './components/Home/Home.jsx'
 import OnBoarding from './components/Onboarding/OnBoarding.jsx'
+import Authentication from './components/Authentication/Authentication.jsx'
 import Popular from './components/Popular/Popular.jsx'
 import Archive from './components/Archive/Archive.jsx'
 import Settings from './components/Settings/Settings.jsx'
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/onboarding" element={<OnBoarding />} />
+        <Route path="/auth" element={<Authentication />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/archive" element={<Archive />} />
         <Route path="/settings" element={<Settings />} />
---
+++ src/components/Authentication/Authentication.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Authentication.sass';
+
+export default function Authentication() {
+    return (
+        <div>Authentication Component</div>
+    )
+}
