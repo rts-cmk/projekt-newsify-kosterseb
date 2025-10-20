ce662ba9aa6c052e507bd52aabb612fc2a097bb7
kosterseb <sebkos.code@gmail.com>
2025-10-20 13:00:54 +0200

    added some startup for onboarding, switching to different branch

--- src/components/Onboarding/OnBoarding.jsx
+++ src/components/Onboarding/OnBoarding.jsx
@@ -3,6 +3,22 @@ import './OnBoarding.sass';
 
 export default function OnBoarding() {
     return (
-        <div>OnBoarding Component</div>
+        <>
+        <div>
+
+            {/* Onboarding page 1 */}
+            <div>
+                <img src="src/assets/first_image.png" alt="Welcome" />
+            </div>
+            {/* Onboarding page 2 */}
+            <div>
+                <img src="src/assets/second_image.png" alt="Features" />
+            </div>
+            {/* Onboarding page 3 */}
+            <div>
+                <img src="src/assets/third_image.png" alt="Get Started" />
+            </div>
+        </div>
+        </>
     )
     }
